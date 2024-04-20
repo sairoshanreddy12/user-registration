6fa2088e8ad65bd2c53f75f205aa6e83ce03685f
sairoshanreddy12 <66123719+sairoshanreddy12@users.noreply.github.com>
2024-04-19 23:46:02 -0500

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://roshan123:roshan123@cluster0.kymg9c8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
